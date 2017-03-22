2a9eeba138462b98cd36240aa5d479fedd20d101
Daniel Brooks <dbrooks@dabsquared.com>
2017-03-22 15:12:40 -0700

    fixing entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 if [ "$1" = 'nginx' ]; then
 	if [ "$APPFILE" ]; then
-       sed -i -e "s/APPFILE/$APPFILE/g" /etc/nginx/sites-available/symfony.conf
+       sed -i -e "s/APPFILE/$APPFILE/g" /etc/nginx/conf.d/symfony.conf
     fi
 
     if [ "$URL" ]; then
-       sed -i -e "s/URL/$URL/g" /etc/nginx/sites-available/symfony.conf
+       sed -i -e "s/URL/$URL/g" /etc/nginx/conf.d/symfony.conf
     fi
 fi
 
